5785aac50d22546f0f1ddebb884e05f9b3b96c89
Szabolcs Barnabás <szabolcs.barnabas@gmail.com>
2021-05-15 02:50:54 +0200

    improves build scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+rm -R build
+rm -R dist
+python3 -m pip install --upgrade build
+python3 -m pip install --upgrade twine
 python3 -m build
 # https://packaging.python.org/guides/using-testpypi/
 twine upload --repository pypi dist/*
--- build_test.sh
+++ build_test.sh
@@ -1,3 +1,5 @@
+python3 -m pip install --upgrade build
+python3 -m pip install --upgrade twine
 python3 -m build
 # https://packaging.python.org/guides/using-testpypi/
 twine upload --repository testpypi dist/*
